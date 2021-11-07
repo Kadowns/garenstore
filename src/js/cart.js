8654f082b1c0b15a1c63e830a740e235cf4d7efa
Ricardo Cicaida Novak <ricardonovak@yahoo.com.br>
2021-11-07 19:50:25 -0300

    -Add cart page functionality

---
+++ src/js/cart.js
@@ -0,0 +1,58 @@
+window.onload = function(){
+    loadCart();
+}
+
+function userHasProductInCart(user, productId){
+    if (user == null){
+        return false
+    }
+
+    for (var i = 0; i < user.cart.length; i++){
+        var productIdOnCart = JSON.parse(user.cart[i])
+
+        if (productIdOnCart == productId){
+            return true;
+        }
+    }
+    return false;
+}
+
+function loadCart(){
+
+    document.getElementById("card-list").innerHTML = "";
+
+    var currentUser = getCurrentUser();
+
+    if (currentUser == null){
+        alert("Please log in a user account")
+        window.location.href = "signin.html";
+        return;
+    }
+
+
+    for(var i = 0; i < currentUser.cart.length; i++) {
+
+        var productId = JSON.parse(currentUser.cart[i]);
+
+        var product = getProductById(productId);
+        
+        var content = buildProductCard(product, buildBuyProductFooter(product, true));
+        
+        document.getElementById("card-list").innerHTML += content;
+    }
+}
+
+function remove(id){
+
+    var currentUser = getCurrentUser();
+    if (currentUser == null){
+        alert("Por favor, realize o login para remover.");
+        return;
+    }
+
+    removeProductFromUser(currentUser, id);
+
+    setCurrentUser(currentUser);
+
+    loadCart();
+}
